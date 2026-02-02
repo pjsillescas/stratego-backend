d53e65e4edd525fef0b611962746000652446fe6
pjsillescas <pjsillescas@gmail.com>
2026-02-02 22:43:41 +0100

    Soporte para CORS.

--- src/main/java/com/pdrosoft/matchmaking/MatchmakingApplication.java
+++ src/main/java/com/pdrosoft/matchmaking/MatchmakingApplication.java
@@ -2,7 +2,10 @@ package com.pdrosoft.matchmaking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { //
@@ -18,6 +21,19 @@ import org.springframework.context.annotation.ComponentScan;
 })
 public class MatchmakingApplication {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                .allowedOrigins("*")
+	                .allowedMethods("*")
+	                .allowedHeaders("*");
+	        }
+	    };
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MatchmakingApplication.class, args);
 	}
